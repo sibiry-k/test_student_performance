4163abf8af22e3e920aabd0b292ccea132226e68
Anton Ivanov <sibiry-k@yandex.ru>
2025-09-24 10:42:32 +0700

    Add result table

--- main.py
+++ main.py
@@ -1,33 +1,37 @@
 import csv
-from config import config_arg_parser
 from pathlib import Path
+
+from config import config_arg_parser
 from reports import report_students_performance
 
-DATA_FOLDER = Path('./data')
+DATA_FOLDER = Path("./data")
+
 
 def read_students_results(filename):
     try:
-        with open(filename, newline='', encoding='utf-8') as file:
-            reader = csv.reader(file, delimiter=',')
-            return list(reader)
+        with open(filename, newline="", encoding="utf-8") as file:
+            csvreader = csv.DictReader(file)
+            return list(csvreader)
     except FileNotFoundError:
         print(f"Файл {filename} не найден.")
         return
+
 
 def main():
     arg_parser = config_arg_parser()
     args = arg_parser.parse_args()
     if args.report == "students-performance":
+        common_data = []
         for filename in args.files:
             file_path = DATA_FOLDER / filename
             input_data = read_students_results(file_path)
-            report_students_performance(input_data)
+            common_data.extend(input_data)
+        report_students_performance(common_data)
     else:
-        print("Укажите вариант отчета. "
-              "Например: '--report students-performance'"
+        print(
+            "Укажите вариант отчета. Например: '--report students-performance'"
         )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-
--- reports.py
+++ reports.py
@@ -1,5 +1,36 @@
 from tabulate import tabulate
 
-def report_students_performance(data):
-    print(tabulate(data))
 
+def report_students_performance(data):
+    table = {}
+
+    # Формируем словарь {Студент: список оценок}
+    for row in data:
+        student_name = row["student_name"]
+        grade = int(row["grade"])
+        table.setdefault(student_name, []).append(grade)
+
+    # Формируем итоговый словарь {Студент: средняя оценка}
+    result_table = {
+        student: sum(grade) / len(grade) for student, grade in table.items()
+    }
+
+    # Сортируем итоговый словарь по средней оценке
+    rows = sorted(
+        ((student, f"{avg:.1f}") for student, avg in result_table.items()),
+        key=lambda x: float(x[1]),
+        reverse=True,
+    )
+
+    # Добавляем счётчик
+    result_rows = [
+        (i, student, avg) for i, (student, avg) in enumerate(rows, start=1)
+    ]
+
+    print(
+        tabulate(
+            result_rows,
+            headers=["", "student_name", "grade"],
+            tablefmt="pretty",
+        )
+    )
