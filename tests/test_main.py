41744e40ad51e24fcca6dd8890d698e1a4f7c33c
Anton Ivanov <sibiry-k@yandex.ru>
2025-09-24 14:47:22 +0700

    Add pytest

---
+++ tests/test_main.py
@@ -0,0 +1,14 @@
+from unittest.mock import mock_open, patch
+
+from main import read_students_results
+
+
+def test_main():
+    assert 1
+
+
+def test_read_students_results_valid(csv_valid):
+    mock_file = mock_open(read_data=csv_valid)
+    with patch("builtins.open", mock_file):
+        data = read_students_results("test_students.csv")
+        assert isinstance(data, list)
