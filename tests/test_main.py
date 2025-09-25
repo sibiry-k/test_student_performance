53e34906ce9c8f60e657e49e4aa744f580a354d2
Anton Ivanov <sibiry-k@yandex.ru>
2025-09-25 17:37:31 +0700

    Add img

--- tests/test_main.py
+++ tests/test_main.py
@@ -21,6 +21,7 @@ class TestReadCSV:
             assert csv_data[2]["grade"] == "5"
 
     def test_returns_none_on_file_not_found(self):
+        """Проверяем обработку ошибки FileNotFoundError."""
         with patch("builtins.open", side_effect=FileNotFoundError):
             result = read_students_results("file_not_found.csv")
         assert result is None
@@ -30,6 +31,7 @@ class TestReportStudentsPerformance:
     """Тесты для проверки составления отчета."""
 
     def test_calc_avg_for_student(self, csv_for_avg_calc_student):
+        """Проверяем правильный подсчет средней оценки одного студента."""
         with patch(
             "builtins.open", mock_open(read_data=csv_for_avg_calc_student)
         ):
@@ -42,6 +44,7 @@ class TestReportStudentsPerformance:
         assert output.count("Семенова Елена") == 1
 
     def test_calc_avg_for_some_students(self, csv_for_avg_calc_some_students):
+        """Проверяем правильный подсчет средней оценки нескольких студентов."""
         with patch(
             "builtins.open",
             mock_open(read_data=csv_for_avg_calc_some_students),
@@ -58,6 +61,7 @@ class TestReportStudentsPerformance:
         assert "3.5" in output
 
     def test_rate_students(self, csv_for_rate_students):
+        """Проверяем правильное ранжирование студентов в отчете."""
         with patch(
             "builtins.open", mock_open(read_data=csv_for_rate_students)
         ):
@@ -77,21 +81,25 @@ class TestConfigModule:
     """Тесты для проверки создания атрибутов командной строки."""
 
     def test_config_arg_parser(self):
+        """Проверяем корректное создание парсера аргументов."""
         parser = config_arg_parser()
         assert parser is not None
         assert parser.prog == "poetry run python main.py"
 
     def test_config_arg_parser_args_files(self):
+        """Проверяем обработку аргумента --files."""
         parser = config_arg_parser()
         args = parser.parse_args(["--files", "test1.csv", "test2.csv"])
         assert args.files == ["test1.csv", "test2.csv"]
 
     def test_config_arg_parser_args_report(self):
+        """Проверяем обработку аргумента --report."""
         parser = config_arg_parser()
         args = parser.parse_args(["--report", "test-student"])
         assert args.report == "test-student"
 
     def test_config_arg_parser_no_args(self):
+        """Проверяем обработку запуска скрипта без аргументов."""
         parser = config_arg_parser()
         args = parser.parse_args([])
         assert args.report is None
@@ -103,6 +111,7 @@ class TestMainModule:
 
     @patch("main.config_arg_parser")
     def test_main_no_args_files(self, mock_parser):
+        """Проверяем обработку ошибки запуска скрипта без аргументов."""
         mock_args = MagicMock()
         mock_args.files = None
         mock_parser.return_value.parse_args.return_value = mock_args
@@ -116,6 +125,7 @@ class TestMainModule:
 
     @patch("main.config_arg_parser")
     def test_main_no_csv_file(self, mock_parser):
+        """Проверяем обработку запуска скрипта с указанием не csv-файла."""
         mock_args = MagicMock()
         mock_args.files = ["test1.txt", "test2.csv"]
         mock_parser.return_value.parse_args.return_value = mock_args
@@ -129,6 +139,7 @@ class TestMainModule:
 
     @patch("main.config_arg_parser")
     def test_main_wrong_report(self, mock_parser):
+        """Проверяем запуск скрипта с неправильным аргументом --report."""
         mock_args = MagicMock()
         mock_args.files = ["test.csv"]
         mock_args.report = ["wrong-report"]
@@ -147,6 +158,7 @@ class TestMainModule:
     def test_main_right_work(
         self, mock_report, mock_read, mock_parser, csv_valid
     ):
+        """Проверяем корректное выполнение скрипта с корректными данными."""
         mock_args = MagicMock()
         mock_args.files = ["test1.csv"]
         mock_args.report = "student-performance"
